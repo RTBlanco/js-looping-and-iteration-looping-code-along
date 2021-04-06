e15cdc274c0d7d94882954aeb89e60ea0fc7c287
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-06 00:43:10 -0400

    commit tree 64f57552fa7cab65ae33f301af39500c3683e646

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function wrtieCards(array, event) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray;
+}
